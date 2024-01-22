c066868e11caba5118aff9d478082d493828f31e
root <adamak.tnh@gmail.com>
2024-01-22 15:12:07 +0330

    added user in admin panel

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import *
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset
+    
+    list_display = ("id", "username", "email")
+    list_filter = ("email",)
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        ('Personal info', {
+            'fields': (
+                'first_name',
+                'last_name', 'coin', 'phone')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_superuser',
+                'groups', 'user_permissions')
+        })
+    )
+
+    search_fields = ('email', 'first_name',"username")
+    ordering = ('-id', 'email')
