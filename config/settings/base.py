b5d8184d44426cab8443078d5eb166210752fcb5
root <adamak.tnh@gmail.com>
2024-01-22 15:12:57 +0330

    added jwt in serializer

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'apps.account',
     'apps.api',
 ]
@@ -110,3 +112,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
+
+# JWT *
+SIMPLE_JWT = {
+  "TOKEN_OBTAIN_SERIALIZER": "apps.api.utils.jwt_customize_token.CustomTokenObtainPairSerializer",
+}
