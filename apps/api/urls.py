559f18b5af2d36ac6417e961d78a0c6b64b99186
root <adamak.tnh@gmail.com>
2024-01-22 15:18:29 +0330

    added urls path

---
+++ apps/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from apps.api.views import *
+from rest_framework import routers
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
+
+
+
+app_name = 'api'
+urlpatterns = []
+
+router = routers.SimpleRouter()
+router.register('auth/register', UserRegister, basename='user-register')
+urlpatterns += [
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] 
+
+urlpatterns += router.urls
