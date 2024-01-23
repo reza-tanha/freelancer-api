1807785291a80db057322b84d07088e17417936f
root <adamak.tnh@gmail.com>
2024-01-23 17:23:33 +0330

    added cache in AdvertisementViewSet

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,10 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 from apps.advertisement.models import Advertisement
 from apps.api.serializers import *
 from apps.api.serializer.advertisment import AdvertisementSerializer
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from django.views.decorators.vary import vary_on_cookie, vary_on_headers
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class UserRegister(ModelViewSet):
@@ -15,26 +19,15 @@ class UserRegister(ModelViewSet):
     authentication_classes = ()
 
 
-# @method_decorator(cache_page(15), name='dispatch')
+@method_decorator(cache_page(15), name='dispatch')
 class AdvertisementViewSet(ModelViewSet):
     serializer_class = AdvertisementSerializer
+    permission_classes = (IsAuthenticated, )
+    http_method_names = ('post', "get", "list")
 
-    CACHE_KEY_PREFIX = "all_advertisements_cache"
-
-    # @method_decorator(cache_page(10))
-    # @method_decorator(cache_page(10, key_prefix=CACHE_KEY_PREFIX))
-    @method_decorator(vary_on_cookie)
-    def list(self, request, format=None, *args, **kwargs):
-    # def list(self, request, *args, **kwargs):
-        print("List View is called!")
-        return super().list(request, *args, **kwargs)
 
     def get_queryset(self):
         print("Queryset is called!")
         return Advertisement.objects.filter(
             status="publish"
         ).select_related("user", "type_adv")
-    
-    # @method_decorator(cache_page(10, key_prefix=CACHE_KEY_PREFIX))
-    # def dispatch(self, request, *args, **kwargs):
-    #     return super().dispatch(request, *args, **kwargs)
