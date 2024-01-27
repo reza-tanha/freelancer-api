9242997d7d55b30f4ee1210c592618ea0a5646e1
root <adamak.tnh@gmail.com>
2024-01-27 13:51:53 +0330

    added static root path

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from decouple import AutoConfig
-
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
